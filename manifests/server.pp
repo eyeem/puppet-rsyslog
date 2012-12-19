f063c1b92130296f674bf9674a29fe649d5e7199
Tom De Vylder <tom@penumbra.be>
2012-12-19 15:52:55 +0100

    Remove redundant cruft

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,21 +2,12 @@
 #
 class rsyslog::server {
 
-  $server             = $::rsyslog::server
-  $server_log_dir     = $::rsyslog::server_log_dir
-  $server_tcp_address = $::rsyslog::server_tcp_address
-  $server_tcp_enable  = $::rsyslog::server_tcp_enable
-  $server_tcp_port    = $::rsyslog::server_tcp_port
-  $server_udp_address = $::rsyslog::server_udp_address
-  $server_udp_enable  = $::rsyslog::server_udp_enable
-  $server_udp_port    = $::rsyslog::server_udp_port
+  if $::rsyslog::server {
 
-  if $server {
-
-    # Detect port conflicts when both TCP and UPD are enabled.
-    if $server_tcp_enable and $server_udp_enable {
-      if $server_tcp_port == $server_udp_port {
-        if $server_tcp_address == $server_udp_address {
+    # Detect port conflicts when both TCP and UDP are enabled.
+    if $::rsyslog::server_tcp_enable and $::rsyslog::server_udp_enable {
+      if $::rsyslog::server_tcp_port == $::rsyslog::server_udp_port {
+        if $::rsyslog::server_tcp_address == $::rsyslog::server_udp_address {
           fail 'Can not bind RSyslog daemon to identical ports.'
         }
       }
